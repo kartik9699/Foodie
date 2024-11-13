20aefb2655c7bee6598a807f518f1424c8ba3c8a
Kartik Mahendra Parab <126079035+kartik9699@users.noreply.github.com>
2024-11-13 21:44:44 +0530

    Update db.js
    add coonection strings

--- frontend/Backend/db.js
+++ frontend/Backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const mongodb=async ()=>{
-await mongoose.connect('mongodb://localhost:27017/GoFood')
+await mongoose.connect('mongodb+srv://KartikP:Kparab123@kartikp.raibh.mongodb.net/GoFood?retryWrites=true&w=majority&appName=KartikP')
   .then(() => console.log('Connected!'));
   const fetch_data= await mongoose.connection.db.collection("Foods");
   const data=await fetch_data.find().toArray();
